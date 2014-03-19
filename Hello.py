455114b08b9d113cc02f4e602961d5ae190a90c6
Ibrahim192 <mohammedibrahim219@yahoo.in>
2014-03-19 13:19:57 +0530

    Adding python file

---
+++ Hello.py
@@ -0,0 +1,8 @@
+x=raw_input("Enter yout name: ")
+print"Hello,there "+x+"!"
+age=input("Enter your age: ")
+if(age>18):
+    print"Welcome!"
+else:
+    print"THis is not suitable for you!"
+    exit
